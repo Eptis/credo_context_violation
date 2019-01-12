c049b68f345510770734b0dec5d27429fa67a15e
Eptis <dennistel90@gmail.com>
2019-01-12 22:39:45 +0100

    update mix file to publish v0.1.0

--- mix.exs
+++ mix.exs
@@ -3,10 +3,13 @@ defmodule ContextViolation.MixProject do
 
   def project do
     [
-      app: :context_violation,
+      app: :credo_context_violation,
       version: "0.1.0",
       elixir: "~> 1.7",
+      licenses: [],
+      description: "Credo check to check function calls that violate context boundaries.",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -22,8 +25,16 @@ defmodule ContextViolation.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Dennis Tel"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/eptis/credo_context_violation"},
+      source_url: "https://github.com/eptis/credo_context_violation"
     ]
   end
 end
